04354f25a377dd3bfda23c7fb757b70407edf7d2
courtneyberry27 <cmberry27@yahoo.com>
2025-03-27 16:56:58 -0400

    final additions

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,3 +1,9 @@
+"""
+Emotion Detection File 
+
+This python script destablishes the logic and api response for emotion detection using user input in the form of text.
+"""
+
 import requests
 import json
 
@@ -9,22 +15,38 @@ def emotion_detector(text_to_analyze):
 
     # make the post request
     response = requests.post(URL, json=input_json, headers=header)
+
+    # format response
     formatted_response = json.loads(response.text)
 
-    emotions = (formatted_response['emotionPredictions'][0])['emotion']
-    anger_score = emotions['anger']
-    disgust_score = emotions['disgust']
-    fear_score = emotions['fear']
-    sadness_score = emotions['sadness']
-    dominant_emotion = max(emotions, key=emotions.get)
-    display_response_format = {
-                            'anger': anger_score,
-                            'disgust': disgust_score,
-                            'fear': fear_score,
-                            'sadness': sadness_score,   
-                            'dominant_emotion': dominant_emotion
-                        }
+    # error handling and formatted response
+    if response.status_code == 400:
+        keys = formatted_response.keys()
+
+        # return None for values if 400 code
+        for key in keys:
+            formatted_response[key] = None
+
+        formatted_response['dominant_emotion'] = None
+    # otherwise format repsponse for output
+    else:
+        emotions = (formatted_response['emotionPredictions'][0])['emotion']
+        anger_score = emotions['anger']
+        disgust_score = emotions['disgust']
+        fear_score = emotions['fear']
+        sadness_score = emotions['sadness']
+        
+        # get dom emotion via max of scores
+        dominant_emotion = max(emotions, key=emotions.get)
+
+        #format response
+        display_response_format = {
+                                'anger': anger_score,
+                                'disgust': disgust_score,
+                                'fear': fear_score,
+                                'sadness': sadness_score,   
+                                'dominant_emotion': dominant_emotion
+                            }
 
     # return formatted response
-    print(display_response_format)
     return display_response_format
--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+"""
+Emotion Detection Server File
+
+This python script defines a flask server for emotion detection using 
+user input in the form of text.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,20 +11,26 @@ app = Flask("Emotion Detector")
 @app.route("/emotionDetector", methods=['GET'], strict_slashes=False)
 
 def format_response():
+    """Analyze text for emotions
+    and return string with emotion scores and the dominant emotion overall."""
+    # get input from app
     input_text = request.args.get('textToAnalyze')
+    # use emotion detector function with input
     response = emotion_detector(input_text)
-
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
     return (
-        f"For the given statement, the system response is 'anger': {response['anger']} "
-        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
-        f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
-        f"The dominant emotion is {response['dominant_emotion']}."
+            f"For the given statement, the system response is 'anger': {response['anger']} "
+            f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+            f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}."
     )
+
 @app.route("/")
 
-# need port to be localhost:5000
 def home():
-    return "Welcome to the Emotion Detection API!"
+    """render index.html file."""
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
